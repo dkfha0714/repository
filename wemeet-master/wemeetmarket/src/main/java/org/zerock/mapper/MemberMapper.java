218c0d443d32011fcbfed3aea017ae1df030d2f5
dkfhs <dkfha714@gmail.com>
2021-02-09 12:40:12 +0900

    깃허브 다시 연동

---
+++ wemeet-master/wemeetmarket/src/main/java/org/zerock/mapper/MemberMapper.java
@@ -0,0 +1,40 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.MemberVO;
+
+public interface MemberMapper {
+	
+
+	public List<MemberVO> getList();
+	
+	@Transactional
+	public void insert(MemberVO member);
+	
+	public MemberVO login(MemberVO member);
+	
+	@Transactional
+	public int delete(Long mno);
+	
+	@Transactional
+	public int update(MemberVO member);
+	
+	@Transactional
+	public MemberVO select(Long mno);
+	
+	public int agree(MemberVO member);
+	
+	public MemberVO result(MemberVO member);
+
+	public MemberVO emailcheck(MemberVO member);
+	
+	public List<MemberVO> getlist();
+
+	public MemberVO read(Long adno);
+	
+	public int acdelete(Long adno);
+	
+}
---
+++ wemeet-master/wemeetmarket/src/main/java/org/zerock/service/MemberService.java
@@ -0,0 +1,35 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.zerock.domain.Criteria;
+import org.zerock.domain.MemberVO;
+
+public interface MemberService {
+
+	public List<MemberVO> getList();
+
+	public void insert(MemberVO member);
+
+	public MemberVO login(MemberVO member);
+
+	public void delete(Long mno);
+
+	public int update(MemberVO member);
+	
+	public MemberVO select(Long mno);
+
+	public void agree(MemberVO member);
+	
+	public MemberVO result(MemberVO member);
+	
+	public MemberVO emailcheck(MemberVO member);
+
+	public List<MemberVO> getlist();
+	
+	public MemberVO read(Long adno);
+
+	public boolean acdelete(Long adno);
+	
+	
+}
